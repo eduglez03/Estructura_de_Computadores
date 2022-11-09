81bd9dea71d5ee80f2ae6460ae88f51764e96739
Eduardo González Gutiérrez <91562871+eduglez03@users.noreply.github.com>
2022-11-09 11:03:41 +0000

    Update compl1.v

--- Practica_2/compl1.v
+++ Practica_2/compl1.v
@@ -1,5 +1,7 @@
 module compl1(output wire [3:0] Out, input wire [3:0] Inp, input wire cpl);
 
   assign Out = cpl ? ~Inp : Inp;
+  
+  // mux2_4 (Out, Inp, ~Inp, cpl); Otra posible forma de implementación
 
 endmodule
